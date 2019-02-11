230c30d1b42f707612e2ab3524389608bb2fab11
chiragkatare <chirag.katare@autoninja.in>
2019-02-11 18:07:18 +0530

    Updates

---
+++ precache-manifest.6df7213c83f027345a0eefc8dd0b24d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48546051b49ca86d2a3",
+    "url": "/static/css/main.73dd7daa.chunk.css"
+  },
+  {
+    "revision": "b48546051b49ca86d2a3",
+    "url": "/static/js/main.b4854605.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a94e9c6e29eb7708009bacca2e7179e4",
+    "url": "/index.html"
+  }
+];
